e108c8c92a51aa4e57f707c06e2683a6de4cd901
Celio Jr <celio.junior@bludata.com.br>
2019-09-09 10:04:16 -0300

    comments

--- fibonacci.exs
+++ fibonacci.exs
@@ -1,9 +1,9 @@
 defmodule Fibonacci do
-  def compute(n) do
+  def compute(n) do # compute/1
     cond do
       n == 0 -> 0
       n == 1 -> 1
-      true -> compute(n - 1) + compute(n - 2)
+      true -> compute(n - 1) + compute(n - 2) # true = else (=! nil false) 
     end
   end
 end
